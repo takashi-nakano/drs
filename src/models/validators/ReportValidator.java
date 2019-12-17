0395d69267cadeda93047f49e83d51140862704d
takashi.nakano <gmcncppbpaucecbh@aol.com>
2019-12-17 15:16:12 +0900

    Add reports show

--- src/models/validators/ReportValidator.java
+++ src/models/validators/ReportValidator.java
@@ -25,17 +25,16 @@ public class ReportValidator {
 
     }
 
-
-    private static String _validateTitle(String title){
-        if(title== null|| title.equals("")){
+    private static String _validateTitle(String title) {
+        if (title == null || title.equals("")) {
             return "タイトルを入力してください。";
         }
         return "";
-     }
+    }
 
     private static String _validateContent(String content) {
-        if(content== null || content.equals("")){
-            return "";
+        if (content == null || content.equals("")) {
+            return "内容を入力してください。";
         }
         return "";
     }
